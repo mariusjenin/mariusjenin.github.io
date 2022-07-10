e25fab9b45be0b1488ac73aba3f6ac5db0bf6ddd
Jenin Marius <marius.jenin@etu.umontpellier.fr>
2022-07-10 15:15:49 +0200

    add some projects and fix the snapping problem

--- js/main.js
+++ js/main.js
@@ -1,107 +1,120 @@
-
 function main() {
 
-(function () {
-   'use strict';
-   
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 60
-            }, 900);
-            return false;
-          }
-        }
-      });
+    (function () {
+        'use strict';
 
-	// affix the navbar after scroll below header
-$('#nav').affix({
-      offset: {
-        top: $('header').height()
-      }
-});	
-
-	// skills chart
-	$(document).ready(function(e) {
-	//var windowBottom = $(window).height();
-	var index=0;
-	$(document).scroll(function(){
-		var top = $('#skills').height()-$(window).scrollTop();
-		if(top<-300){
-			if(index===0){
-			
-				$('.chart').easyPieChart({
-					easing: 'easeOutBounce',
-					onStep: function(from, to, percent) {
-						$(this.el).find('.percent').text(Math.round(percent));
-					}
-				});
-			
-				}
-			index++;
-		}
-	})
-	//console.log(nagativeValue)
-	});
-	
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
+        $('a.page-scroll').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 900);
+                    return false;
+                }
             }
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
+
+        // affix the navbar after scroll below header
+        let nav =$('#nav')
+        nav.affix({
+            offset: {
+                top: $('header').height()
+            }
+        });
+
+        let intro = $(".intro")
+        nav.on( 'affix.bs.affix', function () {
+            console.log('aff');
+            intro.addClass( 'intro-with-nav-bar-free' );
+        } );
+
+        nav.on( 'affixed-top.bs.affix', function () {
+            console.log('unaff');
+            intro.removeClass( 'intro-with-nav-bar-free' );
+        } );
+
+
+        // skills chart
+        // $(document).ready(function (e) {
+        //     //var windowBottom = $(window).height();
+        //     var index = 0;
+        //     $(document).scroll(function () {
+        //         var top = $('#skills').height() - $(window).scrollTop();
+        //         if (top < -300) {
+        //             if (index === 0) {
+        //
+        //                 $('.chart').easyPieChart({
+        //                     easing: 'easeOutBounce',
+        //                     onStep: function (from, to, percent) {
+        //                         $(this.el).find('.percent').text(Math.round(percent));
+        //                     }
+        //                 });
+        //
+        //             }
+        //             index++;
+        //         }
+        //     })
+        //     //console.log(nagativeValue)
+        // });
+
+        // Hide nav on click
+        $(".navbar-nav li a").click(function (event) {
+            // check if window is small enough so dropdown is created
+            var toggle = $(".navbar-toggle").is(":visible");
+            if (toggle) {
+                $(".navbar-collapse").collapse('hide');
+            }
+        });
+
+        // Portfolio isotope filter
+        $(window).load(function () {
+            var $container = $('.portfolio-items');
             $container.isotope({
-                filter: selector,
+                filter: '*',
                 animationOptions: {
                     duration: 750,
                     easing: 'linear',
                     queue: false
                 }
             });
-            return false;
+            $('.cat a').click(function () {
+                $('.cat .active').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+
         });
 
-    });
-	
-	  	
-    // CounterUp
-	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
-			$('span.count').counterUp({
-					delay: 10, // the delay time in ms
-			time: 1500 // the speed time in ms
-			});
-		}
-	});
-	
-  	// Pretty Photo
-	$("a[rel^='project']").prettyPhoto({
-		social_tools: false
-	});	
 
-}());
+        // CounterUp
+        $(document).ready(function ($) {
+            if ($("span.count").length > 0) {
+                $('span.count').counterUp({
+                    delay: 10, // the delay time in ms
+                    time: 1500 // the speed time in ms
+                });
+            }
+        });
+
+        // Pretty Photo
+        $("a[rel^='project']").prettyPhoto({
+            social_tools: false
+        });
+
+    }());
 
 
 }
+
 main();
