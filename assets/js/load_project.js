3fc9076c8c92c575d72fbe0ce9f6d281f7133caa
Jenin Marius <marius.jenin@etu.umontpellier.fr>
2023-05-09 01:16:19 +0200

    modal

--- assets/js/load_project.js
+++ assets/js/load_project.js
@@ -1,5 +1,7 @@
 // window.addEventListener('resize', load_project);
 window.addEventListener('load', load_project);
+let media = []
+let current_index = 0
 
 function load_project() {
     let url = new URL(window.location)
@@ -10,6 +12,7 @@ function load_project() {
         fetch('assets/projects/' + project + '/data_project.json')
             .then((response) => response.json())
             .then((data_project) => {
+                media = []
                 $("#title_project").html(data_project["title"])
                 $("#project_period").html(data_project["period"])
                 let project_github = $("#project_github")
@@ -33,15 +36,16 @@ function load_project() {
                     } else {
                         html_img += `<div class="col-lg-${size}"><div class="portfolio-image">`
                         if (type === "image"){
-                            html_img +=
-                                `<a href="#">
-                                    <img src="${content}" alt="">
-                                </a>`
+                            let index_media = media.length
+                            media.push(["image",content])
+                            html_img += `<img  onclick="display_modal(${index_media})" src="${content}" alt="image_project">`
                         } else if (type === "text") {
                             html_img += `${content}`
                         } else if (type === "video") {
+                            let index_media = media.length
+                            media.push(["video",content])
                             html_img +=
-                                `<video autoplay="" loop="" muted="">
+                                `<video onclick="display_modal(${index_media})" autoplay="" loop="" muted="">
                                     <source src="${content}" type="video/mp4">
                                 </video>`
                         }
@@ -81,4 +85,71 @@ function load_project() {
                 window.location.href = window.location.toString().split("/").slice(0, -1).join("/") + "/projects";
         });
     }
+}
+
+
+function display_modal(index){
+    current_index = index
+    $("body").append(
+        `<div onclick="leave_modal();" id="modal_window" class="d-flex flex-row align-items-center justify-content-between py-2">
+            <div onclick="event.stopPropagation()" class="btn_modal text-center">
+                <img onclick="load_previous()" src="assets/images/modal/left.png" class="icon_btn_modal" alt="left btn">
+            </div>
+            <figure class="d-flex flex-column justify-content-center align-items-center figure_modal">
+                <div id="media_container" class="text-center">
+                    <div class="btn_leave_modal"><img src="assets/images/modal/cross.png" class="icon_btn_modal icon_btn_leave_modal" alt="leave modal"></div>
+                </div>
+                <figcaption onclick="event.stopPropagation()" id="figure_caption" class="text-center text-white"></figcaption>
+            </figure>
+            <div onclick="event.stopPropagation()" class="btn_modal text-center">
+                <img onclick="load_next()" src="assets/images/modal/right.png" class="icon_btn_modal" alt="left btn">
+            </div>
+        </div>`)
+    load_media()
+}
+
+function load_media(){
+    let data = media[current_index]
+    let type = data[0]
+    let url = data[1]
+    $("#media_modal").remove()
+    if(type === "image"){
+        media_html = `<img onclick="event.stopPropagation()" src="${url}" id="media_modal" alt="modal image">`
+    } else{
+        media_html = `<video onclick="event.stopPropagation()" id="media_modal" autoplay="" loop="" muted="">
+                        <source src="${url}" type="video/mp4">
+                    </video>`
+    }
+    $("#media_container").append(media_html);
+    let nb_media = media.length
+    if(nb_media > 1){
+        $("#figure_caption").html((current_index+1)+" of "+nb_media)
+    } else {
+        $("#figure_caption").html("")
+    }
+}
+
+function load_previous(){
+    let index = current_index-1
+    if(index<0){
+        current_index = media.length-1
+    } else {
+        current_index = index
+    }
+    load_media()
+}
+
+function load_next(){
+    let index = current_index+1
+    if(index>=media.length){
+        current_index = 0
+    } else {
+        current_index = index
+    }
+    load_media()
+}
+
+function leave_modal(){
+    $("#modal_window").remove()
+    current_index = 0
 }
